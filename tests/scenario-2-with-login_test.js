8cb101d88a981c9fe5c652654e2138c8087d4cab
William Ong <wong@octo.com>
2017-03-03 17:52:38 +0100

    Change browser to chrome to get it work and change install of selenium

--- tests/scenario-2-with-login_test.js
+++ tests/scenario-2-with-login_test.js
@@ -9,7 +9,7 @@ Scenario('login to micro from homepage', (I) => {
   I.click('.login-form__identificate');
   I.wait(1);
   I.fillField('.message-input__content', 'We are the best team ever!');
-  I.pressKey('.message-input__content', 13);
-  I.wait(10);
+  I.pressKey('Enter');
+  I.wait(1);
   I.see('We are the best team ever!');
 });
