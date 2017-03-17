e54a5a01d5481b89414ca5e6540720424a3e692f
William Ong <wong@octo.com>
2017-03-17 15:18:08 +0100

    Update test to add logout scenario

--- tests/scenario-2-with-login_test.js
+++ tests/scenario-2-with-login_test.js
@@ -1,15 +1,22 @@
 
 Feature('Homepage login page');
 
-Scenario('login to micro from homepage', (I) => {
+Scenario('login to micro from homepage, write a message and logout', (I) => {
+  //Login
   I.amOnPage('/');
   I.seeInCurrentUrl('/login');
   I.see('S\'identifier');
   I.fillField('.login-form__username', 'piou-piou');
-  I.click('.login-form__identificate');
+  I.click('.login-form__identify');
   I.wait(1);
+
+  //Write a message
   I.fillField('.message-input__content', 'We are the best team ever!');
   I.pressKey('Enter');
   I.wait(1);
   I.see('We are the best team ever!');
+
+  //Logout
+  I.click('.navigation-bar__link--logout');
+  
 });
